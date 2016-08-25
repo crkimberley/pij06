f4daf29cdfefbf26c2495489efb579a51e3eec9a
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-08-25 11:07:36 +0100

    Matrix - added missing semicolon

--- src/Matrix.java
+++ src/Matrix.java
@@ -2,13 +2,13 @@
  * @author crkimberley on 25/08/2016.
  */
 public class Matrix {
-    private int[][] matrix
+    private int[][] matrix;
 
     public Matrix(int x, int y) {
-        matrix = new int[x][y]
+        matrix = new int[x][y];
         for (int i=0; i<x; i++) {
             for (int j=0; j<y; j++) {
-                matrix[i][j] = 1
+                matrix[i][j] = 1;
             }
         }
     }
