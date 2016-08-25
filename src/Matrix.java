bdfcfd639d72199796fe77035f420afc5e364464
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-08-25 11:06:30 +0100

    Matrix - constructor added

--- src/Matrix.java
+++ src/Matrix.java
@@ -2,5 +2,16 @@
  * @author crkimberley on 25/08/2016.
  */
 public class Matrix {
+    private int[][] matrix
+
+    public Matrix(int x, int y) {
+        matrix = new int[x][y]
+        for (int i=0; i<x; i++) {
+            for (int j=0; j<y; j++) {
+                matrix[i][j] = 1
+            }
+        }
+    }
+
 
 }
