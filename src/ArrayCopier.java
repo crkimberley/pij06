cb29d3f6a1618225711792b0a6096f1cbee13976
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-08-25 02:37:09 +0100

    ArrayCopier &  script - working fine

---
+++ src/ArrayCopier.java
@@ -0,0 +1,11 @@
+/**
+ * @author crkimberley on 25/08/2016.
+ */
+public class ArrayCopier {
+
+    public void copy(int[] src, int[] dst) {
+        for (int i=0; i<dst.length; i++) {
+            dst[i] = (i < src.length ? src[i] : 0);
+        }
+    }
+}
---
+++ src/ArrayCopierScript.groovy
@@ -0,0 +1,14 @@
+/**
+ * @author crkimberley on 25/08/2016.
+ */
+ArrayCopier copier = new ArrayCopier()
+int[] arrayDst = new int[10];
+arraySrcSmaller = [1, 2 ,3, 4, 5] as int[]
+arraySrcEqual = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20] as int[]
+arraySrcBigger = [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33] as int[]
+copier.copy(arraySrcSmaller, arrayDst)
+println arrayDst
+copier.copy(arraySrcEqual, arrayDst)
+println arrayDst
+copier.copy(arraySrcBigger, arrayDst)
+println arrayDst
