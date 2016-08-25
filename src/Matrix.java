ecdde9d00cea3fa2d559aa46fccb3ab3aada4520
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-08-25 15:11:27 +0100

    Matrix & Script - working fine

--- src/Matrix.java
+++ src/Matrix.java
@@ -13,5 +13,55 @@ public class Matrix {
         }
     }
 
+    private void setElement(int x, int y, int value) {
+        if ((x >= 0 && x < matrix.length && y >= 0 && y <  matrix[0].length)) {
+            matrix[x][y] = value;
+        }
+    }
 
+    private void setRow(int x, String rowString) {
+        String[] rowNumbers = rowString.split(",");
+        if (x >= 0 && x < matrix.length && rowNumbers.length == matrix[x].length) {
+            for (int i=0; i < matrix[x].length; i++) {
+                matrix[x][i] = Integer.parseInt(rowNumbers[i]);
+            }
+        }
+    }
+
+    private void setColumn(int y, String columnString) {
+        String[] columnNumbers = columnString.split(",");
+        if (y >= 0 && y < matrix[0].length && columnNumbers.length == matrix.length) {
+            for (int i=0; i<matrix.length; i++) {
+                matrix[i][y] = Integer.parseInt(columnNumbers[i]);
+            }
+        }
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        for (int i=0; i<matrix.length; i++) {
+            for (int j=0; j<matrix[0].length; j++) {
+                sb.append(matrix[i][j]);
+                if (j == matrix[0].length - 1) {
+                    if (i == matrix.length - 1) {
+                        sb.append("]");
+                    } else {
+                        sb.append(";");
+                    }
+                } else {
+                    sb.append(",");
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    private void prettyPrint() {
+        for (int i=0; i<matrix.length; i++) {
+            for (int j=0; j<matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
 }
---
+++ src/MatrixScript.groovy
@@ -0,0 +1,17 @@
+/**
+ * @author crkimberley on 25/08/2016.
+ */
+Matrix testMatrix = new Matrix(3, 4)
+testMatrix.setRow(0, "0,1,2,3")
+testMatrix.setRow(1, "10,11,12,13")
+testMatrix.setRow(2, "20,21,22,23")
+println (testMatrix)
+testMatrix.prettyPrint()
+testMatrix.setColumn(2, "90,91,92")
+println (testMatrix)
+testMatrix.prettyPrint()
+testMatrix.setElement(2, 3, 777)
+println (testMatrix)
+testMatrix.prettyPrint()
+Matrix matrixOfOnes = new Matrix(15, 17)
+matrixOfOnes.prettyPrint()
